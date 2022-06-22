4e98e6295e8dfc0c4547afb9a8443e6a71ff84ae
JiLemka <antonmefedov@icloud.com>
2022-06-22 14:20:18 +0300

    Initial commit

---
+++ app/src/main/java/com/example/personalbudgetingapp/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.personalbudgetingapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CardView budgetCardView, todayCardView;
+
+    private ImageView weekBtnImageView, todayBtnImageView, monthBtnImageView, budgetBtnImageView, analyticsImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        weekBtnImageView = findViewById(R.id.weekBtnImageView);
+        todayBtnImageView = findViewById(R.id.todayBtnImageView);
+        budgetBtnImageView = findViewById(R.id.budgetBtnImageView);
+        monthBtnImageView = findViewById(R.id.monthBtnImageView);
+        analyticsImageView = findViewById(R.id.analyticsImageView);
+
+        budgetBtnImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, BudgetActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        todayBtnImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TodaySpendingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        weekBtnImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, WeekSpendingActivity.class);
+                intent.putExtra("type", "week");
+                startActivity(intent);
+            }
+        });
+
+        monthBtnImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, WeekSpendingActivity.class);
+                intent.putExtra("type", "month");
+                startActivity(intent);
+            }
+        });
+
+        analyticsImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ChooseAnalyticActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/personalbudgetingapp/WeeklyAnalyticsActivity.java
@@ -0,0 +1,89 @@
+package com.example.personalbudgetingapp;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class WeeklyAnalyticsActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private String onlineUserId = "";
+    private DatabaseReference expensesRef, personalRef;
+
+    private TextView totalBudgetAmountTextView, analyticsTransportAmount, analyticsFoodAmount, analyticsHouseExpensesAmount;
+    private TextView analyticsEducationAmount, analyticsCharityAmount, analyticsApparelAmount, analyticsHealthAmount;
+    private TextView analyticsPersonalExpensesAmount, analyticsOtherAmount;
+
+    private RelativeLayout linearLayoutTransport, linearLayoutFood, linearLayoutFoodHouse, linearLayoutEntertainment;
+    private RelativeLayout linearLayoutEducation, linearLayoutCharity, linearLayoutApparel, linearLayoutHealth;
+    private RelativeLayout linearLayoutPersonalExp, linearLayoutOther;
+
+    private TextView progress_ratio_transport, progress_ratio_food, progress_ratio_house, progress_ratio_ent, progress_ratio_edu;
+    private TextView progress_ratio_cha, progress_ratio_app, progress_ratio_hea, progress_ratio_per, progress_ratio_oth;
+
+    private ImageView status_Image_transport, status_Image_food, status_Image_house, status_Image_ent, status_Image_edu;
+    private ImageView status_Image_cha, status_Image_app, status_Image_hea, status_Image_per, status_Image_oth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weekly_analytics);
+
+        mAuth = FirebaseAuth.getInstance();
+        onlineUserId = mAuth.getCurrentUser().getUid();
+        expensesRef = FirebaseDatabase.getInstance().getReference("expenses").child(onlineUserId);
+        personalRef = FirebaseDatabase.getInstance().getReference("personal").child(onlineUserId);
+
+        totalBudgetAmountTextView = findViewById(R.id.totalBudgetAmountTextView);
+        analyticsTransportAmount = findViewById(R.id.analyticsTransportAmount);
+        analyticsFoodAmount = findViewById(R.id.analyticsFoodAmount);
+        analyticsHouseExpensesAmount = findViewById(R.id.analyticsHouseExpensesAmount);
+        analyticsEducationAmount = findViewById(R.id.analyticsEducationAmount);
+        analyticsCharityAmount = findViewById(R.id.analyticsCharityAmount);
+        analyticsApparelAmount = findViewById(R.id.analyticsApparelAmount);
+        analyticsHealthAmount = findViewById(R.id.analyticsHealthAmount);
+        analyticsPersonalExpensesAmount = findViewById(R.id.analyticsPersonalExpensesAmount);
+        analyticsOtherAmount = findViewById(R.id.analyticsOtherAmount);
+
+        linearLayoutTransport = findViewById(R.id.linearLayoutTransport);
+        linearLayoutFood = findViewById(R.id.linearLayoutFood);
+        linearLayoutFoodHouse = findViewById(R.id.linearLayoutFoodHouse);
+        linearLayoutEntertainment = findViewById(R.id.linearLayoutEntertainment);
+        linearLayoutEducation = findViewById(R.id.linearLayoutEducation);
+        linearLayoutCharity = findViewById(R.id.linearLayoutCharity);
+        linearLayoutApparel = findViewById(R.id.linearLayoutApparel);
+        linearLayoutHealth = findViewById(R.id.linearLayoutHealth);
+        linearLayoutPersonalExp = findViewById(R.id.linearLayoutPersonalExp);
+        linearLayoutOther = findViewById(R.id.linearLayoutOther);
+
+        progress_ratio_transport = findViewById(R.id.progress_ratio_transport);
+        progress_ratio_food = findViewById(R.id.progress_ratio_food);
+        progress_ratio_house = findViewById(R.id.progress_ratio_house);
+        progress_ratio_ent = findViewById(R.id.progress_ratio_ent);
+        progress_ratio_edu = findViewById(R.id.progress_ratio_edu);
+        progress_ratio_cha = findViewById(R.id.progress_ratio_cha);
+        progress_ratio_app = findViewById(R.id.progress_ratio_app);
+        progress_ratio_hea = findViewById(R.id.progress_ratio_hea);
+        progress_ratio_per = findViewById(R.id.progress_ratio_per);
+        progress_ratio_oth = findViewById(R.id.progress_ratio_oth);
+
+        status_Image_transport = findViewById(R.id.status_Image_transport);
+        status_Image_food = findViewById(R.id.status_Image_food);
+        status_Image_house = findViewById(R.id.status_Image_house);
+        status_Image_ent = findViewById(R.id.status_Image_ent);
+        status_Image_edu = findViewById(R.id.status_Image_edu);
+        status_Image_cha = findViewById(R.id.status_Image_cha);
+        status_Image_app = findViewById(R.id.status_Image_app);
+        status_Image_hea = findViewById(R.id.status_Image_hea);
+        status_Image_per = findViewById(R.id.status_Image_per);
+        status_Image_oth = findViewById(R.id.status_Image_oth);
+    }
+}
